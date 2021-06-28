ed548ec15f7d063e30392a7aac9d29fd2d3e0fe1
Andre Baltieri <andrebaltieri@hotmail.com>
2021-06-28 14:31:45 -0300

    Enabling swagger

--- Startup.cs
+++ Startup.cs
@@ -36,9 +36,10 @@ namespace BaltaShop.Products
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "BaltaShop.Products v1"));
+                app.UseSwagger();                
             }
+            
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "BaltaShop.Products v1"));
 
             app.UseHttpsRedirection();
 
